3f1624840de582f9f573b7fa81cc8c34f5f5c380
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2025-06-26 15:47:29 +0200

    refactor: AddExpenses make explicit

--- src/components/AddExpense.tsx
+++ src/components/AddExpense.tsx
@@ -30,69 +30,71 @@ const AddExpense: React.FC<AddExpenseProps> = ({
   newExpense,
   setNewExpense,
   addExpense,
-}) => (
-  <Grid>
-    <Card>
-      <CardContent>
-        <Typography variant='h6' mb={2}>
-          Add Expense
-        </Typography>
-        <FormControl fullWidth margin='normal'>
-          <InputLabel>Category</InputLabel>
-          <Select
-            value={newExpense.category}
+}) => {
+  return (
+    <Grid>
+      <Card>
+        <CardContent>
+          <Typography variant='h6' mb={2}>
+            Add Expense
+          </Typography>
+          <FormControl fullWidth margin='normal'>
+            <InputLabel>Category</InputLabel>
+            <Select
+              value={newExpense.category}
+              onChange={(e) =>
+                setNewExpense({ ...newExpense, category: e.target.value })
+              }
+              label='Category'
+            >
+              {categories.map((cat) => (
+                <MenuItem key={cat} value={cat}>
+                  {cat}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <TextField
+            fullWidth
+            margin='normal'
+            type='number'
+            label='Amount'
+            value={newExpense.amount}
             onChange={(e) =>
-              setNewExpense({ ...newExpense, category: e.target.value })
+              setNewExpense({
+                ...newExpense,
+                amount: Number(e.target.value),
+              })
             }
-            label='Category'
+          />
+          <TextField
+            fullWidth
+            margin='normal'
+            label='Note'
+            value={newExpense.note}
+            onChange={(e) =>
+              setNewExpense({ ...newExpense, note: e.target.value })
+            }
+          />
+          <DatePicker
+            label='Date'
+            value={newExpense.date}
+            onChange={(date) =>
+              setNewExpense({ ...newExpense, date: date || new Date() })
+            }
+          />
+          <Button
+            variant='contained'
+            fullWidth
+            sx={{ mt: 2 }}
+            onClick={addExpense}
           >
-            {categories.map((cat) => (
-              <MenuItem key={cat} value={cat}>
-                {cat}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <TextField
-          fullWidth
-          margin='normal'
-          type='number'
-          label='Amount'
-          value={newExpense.amount}
-          onChange={(e) =>
-            setNewExpense({
-              ...newExpense,
-              amount: Number(e.target.value),
-            })
-          }
-        />
-        <TextField
-          fullWidth
-          margin='normal'
-          label='Note'
-          value={newExpense.note}
-          onChange={(e) =>
-            setNewExpense({ ...newExpense, note: e.target.value })
-          }
-        />
-        <DatePicker
-          label='Date'
-          value={newExpense.date}
-          onChange={(date) =>
-            setNewExpense({ ...newExpense, date: date || new Date() })
-          }
-        />
-        <Button
-          variant='contained'
-          fullWidth
-          sx={{ mt: 2 }}
-          onClick={addExpense}
-        >
-          + Add Expense
-        </Button>
-      </CardContent>
-    </Card>
-  </Grid>
-);
+            + Add Expense
+          </Button>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
 
 export default AddExpense;
